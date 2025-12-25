07407e39cee62ebbd2792cb041bd8d1c76983558
arunkjojo <arunkjojo@gmail.com>
2025-12-25 16:14:36 +0530

    Add the resume

--- constants/resume.ts
+++ constants/resume.ts
@@ -2,5 +2,5 @@ export const RESUME_CONTENT = {
     title: "My Resume",
     description: "View my professional journey and technical expertise. Download the resume for offline viewing.",
     downloadButton: "Download Resume",
-    fileName: "arun_jojo_software_engineer_resume.pdf" // expecting this file in public/
+    fileName: "Arun_Jojo_Senior_Software_Engineer_Resume.pdf" // expecting this file in public/
 };
